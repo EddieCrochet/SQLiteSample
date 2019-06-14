f5235e484439e96d52c9113d8e6284835918953b
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-06-14 10:09:32 -0500

    created initial console app

---
+++ SQLiteSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQLiteSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
